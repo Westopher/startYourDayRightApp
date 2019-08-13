a072b5b79397de253eaf93fff1cf7c83d6b06ac1
West Kraemer <kraemerwest@gmail.com>
2019-08-13 15:20:32 +0200

    add vc's for vews, sports, and weather tabs

---
+++ startYourDayRightApp/Controllers/NewsVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewsVC.swift
+//  startYourDayRightApp
+//
+//  Created by West Kraemer on 8/13/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class NewsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
