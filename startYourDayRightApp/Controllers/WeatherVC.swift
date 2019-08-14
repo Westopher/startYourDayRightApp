21c50c11dbdff251e51e2fb62dafc7a1ae8e4d36
West Kraemer <kraemerwest@gmail.com>
2019-08-14 14:04:04 +0200

    need to set up the models first. But began setting up the decoding of the JSON anyway. That's okay, it'll be there when I get back to it.

--- startYourDayRightApp/Controllers/WeatherVC.swift
+++ startYourDayRightApp/Controllers/WeatherVC.swift
@@ -12,13 +12,31 @@ class WeatherVC: UIViewController {
     
     @IBOutlet weak var tempDisplayLabel: UILabel!
     
-    func createUrl() {
-        var latitude: String = ""
-        var longitude: String = ""
-        var sampleAPICall = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=61fe2713c932b2a01162cf784e550f91"
-        var appID = "61fe2713c932b2a01162cf784e550f91"
     
+    func getWeatherJSON() {
+        //var latitude: String = ""
+        //var longitude: String = ""
+        var sampleAPICall = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=61fe2713c932b2a01162cf784e550f91"
+        //var appID = "61fe2713c932b2a01162cf784e550f91"
+        
         let weatherURL = "\(sampleAPICall)"
+        
+        guard let url = URL(string: sampleAPICall) else { return print("could not get url") }
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url) { (data, response, error) in
+            if error == nil && data != nil {
+                do {
+                    let decoder = JSONDecoder()
+                    let result = 
+                    
+                } catch {
+                    print("could not decode the JSON")
+                    return
+                }
+            }
+        }
+        
+        
     }
     
     override func viewDidLoad() {
