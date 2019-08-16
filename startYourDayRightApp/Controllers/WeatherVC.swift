d2c723edaa92b98fb4977ecff36301f80915e5bc
West Kraemer <kraemerwest@gmail.com>
2019-08-16 17:21:22 +0200

    update weather icon to include safety control

--- startYourDayRightApp/Controllers/WeatherVC.swift
+++ startYourDayRightApp/Controllers/WeatherVC.swift
@@ -86,55 +86,17 @@ class WeatherVC: UIViewController {
                             print("no description")
                         }
                         
-                        //weather icon enum
-                        func updateWeatherIcon(condition: Int) -> String {
-                            switch (condition) {
-                                
-                            case 0...300 :
-                                return "tstorm1"
-                                
-                            case 301...500 :
-                                return "light_rain"
-                                
-                            case 501...600 :
-                                return "shower3"
-                                
-                            case 601...700 :
-                                return "snow4"
-                                
-                            case 701...771 :
-                                return "fog"
-                                
-                            case 772...799 :
-                                return "tstorm3"
-                                
-                            case 800 :
-                                return "sunny"
-                                
-                            case 801...804 :
-                                return "cloudy2"
-                                
-                            case 900...903, 905...1000  :
-                                return "tstorm3"
-                                
-                            case 903 :
-                                return "snow5"
-                                
-                            case 904 :
-                                return "sunny"
-                                
-                            default :
-                                return "no data available for these conditions"
-                            }
-                            
-                        }
-                        
+                        //weather icon
                         var weatherIconName = ""
                         var condition: Int = 0
                         
-                        condition = (result.weather?[0].id)!
-                        weatherIconName = updateWeatherIcon(condition: condition)
-                        self.weatherPictureView.image = UIImage(named: weatherIconName)
+                        if result.weather?[0].id != nil {
+                            condition = (result.weather?[0].id)!
+                            weatherIconName = self.updateWeatherIcon(condition: condition)
+                            self.weatherPictureView.image = UIImage(named: weatherIconName)
+                        } else {
+                            print("no weather id for icon")
+                        }
                         
                         
 //                        updateWeatherIcon(condition: weatherID!)
@@ -168,4 +130,49 @@ class WeatherVC: UIViewController {
     }
     
 
+}
+
+
+extension WeatherVC {
+    func updateWeatherIcon(condition: Int) -> String {
+        switch (condition) {
+            
+        case 0...300 :
+            return "tstorm1"
+            
+        case 301...500 :
+            return "light_rain"
+            
+        case 501...600 :
+            return "shower3"
+            
+        case 601...700 :
+            return "snow4"
+            
+        case 701...771 :
+            return "fog"
+            
+        case 772...799 :
+            return "tstorm3"
+            
+        case 800 :
+            return "sunny"
+            
+        case 801...804 :
+            return "cloudy2"
+            
+        case 900...903, 905...1000  :
+            return "tstorm3"
+            
+        case 903 :
+            return "snow5"
+            
+        case 904 :
+            return "sunny"
+            
+        default :
+            return "no data available for these conditions"
+        }
+        
+    }
 }
