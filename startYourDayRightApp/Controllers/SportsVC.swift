239beb70bbe2bebddae5e6521839879e0e2d9f51
West Kraemer <kraemerwest@gmail.com>
2019-08-16 17:12:21 +0200

    create tableview for sports tab and populate it with sample array

--- startYourDayRightApp/Controllers/SportsVC.swift
+++ startYourDayRightApp/Controllers/SportsVC.swift
@@ -10,21 +10,30 @@ import UIKit
 
 class SportsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var array = ["1","2","3"]
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+
+
+extension SportsVC: UITableViewDelegate {
+    
+}
+
+extension SportsVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "sportsArticleCell", for: indexPath)
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
     }
-    */
-
+    
+    
 }
